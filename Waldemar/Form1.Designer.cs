6a1d8c74446c04e600377b373fff4cb67727aa2d
xdev_pl <xdev_pl@xelto.com>
2025-01-22 18:49:39 +0100

    Add project files.

---
+++ Waldemar/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+namespace Waldemar
+{
+    partial class Form1 : Form
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        /// 
+        private bool isQPressed = false;
+        private bool isWPressed = false;
+        private bool isEPressed = false;
+
+
+        protected override void OnClosed(EventArgs e)
+        {
+            UnhookWindowsHookEx(_hookID);
+            base.OnClosed(e);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (vkCode == (int)Keys.LWin || vkCode == (int)Keys.RWin)
+                {
+                    // Zablokuj klawisz Windows
+                    return (IntPtr)1;
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+
+            this.BackgroundImage = Properties.Resources.waldus;
+            this.BackgroundImageLayout = ImageLayout.Stretch; // Opcjonalnie, dostosuj do własnych potrzeb
+
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.laugh);
+            player.PlayLooping();
+
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+
+            // Ustawienia dla pełnego ekranu i blokady
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.TopMost = true;
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            this.KeyUp += new KeyEventHandler(Form1_KeyUp);
+            _hookID = SetHook(_proc);
+
+
+            // Zdarzenie utrzymania focusu
+            this.Deactivate += new EventHandler(Form1_Deactivate);
+
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            this.Activate();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!isQPressed || !isWPressed || !isEPressed)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Q) isQPressed = true;
+            if (e.KeyCode == Keys.W) isWPressed = true;
+            if (e.KeyCode == Keys.E) isEPressed = true;
+
+            if (isQPressed && isWPressed && isEPressed)
+            {
+                this.Close();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Q) isQPressed = false;
+            if (e.KeyCode == Keys.W) isWPressed = false;
+            if (e.KeyCode == Keys.E) isEPressed = false;
+        }
+    }
+    #endregion
+}
